374d239be16a1df14ffa584c762d7adc83d158a6
cactusnix <cocalemontea@gmail.com>
2022-09-14 15:37:59 +0800

    feat: add subinfo panels js

--- Panels/SubInfo.js
+++ Panels/SubInfo.js
@@ -1,7 +1,54 @@
 /**
  * Generate subscription info with the url you input and display on the panel
- * Updated: Sep 7, 2022
+ * Updated: Sep 14, 2022
  * @Author cactusnix
  */
-$httpClient.get("", () => {
-})
+const argument = $argument;
+if (!argument) {
+  $done({
+    title: "No $argument!",
+  });
+}
+const args = Object.fromEntries(
+  $argument
+    .split("&")
+    .map((it) => it.split("=").map(([k, v]) => [k, decodeURIComponent(v)]))
+);
+$httpClient.get(args.url, (err, resp, _) => {
+  if (err || resp.status !== 200 || !resp.headers["subscription-userinfo"]) {
+    $done({
+      title: "Get Info Error or no info in headers",
+    });
+  }
+  const data = resp.headers["subscription-userinfo"];
+  const keyValue = {
+    Up: {
+      regex: /upload=(\d+)/,
+      value: 0,
+    },
+    Down: {
+      regex: /download=(\d+)/,
+      value: 0,
+    },
+    Total: {
+      regex: /total=(\d+)/,
+      value: 0,
+    },
+  };
+  Object.keys(keyValue).forEach((key) => {
+    keyValue[key].value = Number(
+      (data.match(keyValue[key].regex)[1] / (1024 * 1024 * 1024)).toFixed(2)
+    );
+  });
+  const content = [
+    `Used: ${(keyValue.Up.value + keyValue.Down.value).toFixed(2)}GB`,
+    `Total: ${keyValue.Total.value}GB`,
+    `Expire: ${new Date(
+      Number(data.match(/expire=(\d+)/)[1]) * 1000
+    ).toLocaleDateString()}`,
+  ];
+  $done({
+    title: "Sub Info",
+    content: content.join("\n"),
+  });
+});
