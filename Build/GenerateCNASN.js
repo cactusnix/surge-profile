5adbf6e09b9d9fd8914f7f8f0079af24310f97ae
cactusnix <cocalemontea@gmail.com>
2022-09-15 18:53:36 +0800

    feat: remove useless rules to make it simple

--- Build/GenerateCNASN.js
+++ Build/GenerateCNASN.js
@@ -1,16 +1,22 @@
+const axios = require("axios");
 const cheerio = require("cheerio");
-const a = fetch(
-  "https://bgp.he.net/search?search%5Bsearch%5D=Google&commit=Search"
-).then((resp) => {
-  return resp.text();
+axios.get("https://bgp.he.net/country/CN").then((resp) => {
+  const $ = cheerio.load(resp.data);
+  console.log("[$]", $);
 });
-a.then((value) => {
-  console.log("[resp]", value);
-});
-console.log(
-  decodeURIComponent(
-    "https://bgp.he.net/search?search%5Bsearch%5D=Google&commit=Search"
-  )
-);
 
-console.log("https://bgp.he.net/country/CN");
+// const a = fetch(
+//   "https://bgp.he.net/search?search%5Bsearch%5D=Google&commit=Search"
+// ).then((resp) => {
+//   return resp.text();
+// });
+// a.then((value) => {
+//   console.log("[resp]", value);
+// });
+// console.log(
+//   decodeURIComponent(
+//     "https://bgp.he.net/search?search%5Bsearch%5D=Google&commit=Search"
+//   )
+// );
+
+// console.log("https://bgp.he.net/country/CN");
