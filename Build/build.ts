f4e73c0ca2b97806ba2dd8dd2f5b507ed9894794
cactusnix <cocalemontea@gmail.com>
2022-09-19 17:22:10 +0800

    feat: add new build via bun

--- Build/build.ts
+++ Build/build.ts
@@ -1,13 +1,25 @@
 import { resolve, parse, join } from "path";
-import Netflix from "./assets/Netflix.json";
 import { write } from "bun";
+import General from "./assets/General.json";
+import GitHub from "./assets/GitHub.json";
+import Google from "./assets/Google.json";
+import Netflix from "./assets/Netflix.json";
+import Reddit from "./assets/Reddit.json";
+import Telegram from "./assets/Telegram.json";
+import Twitter from "./assets/Twitter.json";
+import YouTube from "./assets/YouTube.json";
 
-const bunWrite = (fileName: string, data: Record<string, string[]>) => {
-  // fix path may garbled, it is bug for bun
-  const destination = join(parse(resolve("../Rules")).ext, `${fileName}.list`);
-  console.log(`[Start]: Building ${fileName}.list ......`);
-  console.log("[Destination]:", destination);
-  const rules: string[] = [];
+type RuleObj = {
+  info: {
+    Total: number;
+    DOMAIN: number;
+    "DOMAIN-SUFFIX": number;
+  };
+  value: string[];
+};
+
+const parseDataToRuleObj = (data: Record<string, string[]>): RuleObj => {
+  const value: string[] = [];
   const info = {
     Total: 0,
     DOMAIN: 0,
@@ -17,23 +29,42 @@ const bunWrite = (fileName: string, data: Record<string, string[]>) => {
     info.Total += v.length;
     info[k] += v.length;
     v.forEach((it) => {
-      rules.push(`${k}, ${it}`);
+      value.push(`${k}, ${it}`);
     });
   });
+  return {
+    info,
+    value,
+  };
+};
+
+const bunWrite = (fileName: string, obj: RuleObj) => {
+  // fix path may garbled, it is bug for bun
+  const destination = join(parse(resolve("../Rules")).ext, `${fileName}.list`);
+  console.log(`[Start]: Building ${fileName}.list ......`);
+  console.log("[Destination]:", destination);
   write(
     destination,
     [
       `# Name: ${fileName} Rules`,
-      ...Object.entries(info).map(([k, v]) => {
+      ...Object.entries(obj.info).map(([k, v]) => {
         return `# ${k}: ${v}`;
       }),
       `# Updated: ${new Date().toString()}\n`,
-      ...rules,
+      ...obj.value,
     ].join("\n")
   );
 };
-const buildNetflix = () => {
-  bunWrite("Netflix", Netflix);
+
+const buildTelegram = async () => {
+  // Telegram doesn't have domain for service, so use ASN
+  const obj = parseDataToRuleObj(Telegram);
 };
 
-buildNetflix();
+bunWrite("GitHub", parseDataToRuleObj(GitHub));
+bunWrite("Google", parseDataToRuleObj(Google));
+bunWrite("Netflix", parseDataToRuleObj(Netflix));
+bunWrite("Reddit", parseDataToRuleObj(Reddit));
+buildTelegram();
+bunWrite("Twitter", parseDataToRuleObj(Twitter));
+bunWrite("YouTube", parseDataToRuleObj(YouTube));
