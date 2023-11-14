b02c81ed794f1caf610870c02cfa1ad6e71f655a
cactusnix <cocalemontea@gmail.com>
2023-11-14 14:59:48 +0800

    feat: remove useless list & generate latest

--- Build/build.ts
+++ Build/build.ts
@@ -1,16 +1,20 @@
 import { resolve, parse, join } from "path";
 import { write } from "bun";
-import China from "./assets/China.json";
-import Block from "./assets/Block.json";
-import Netflix from "./assets/Netflix.json";
-import Google from "./assets/Google.json";
+/**
+ * General Parts
+ * It's not include the special parts
+ */
+import Google from "./assets/general/Google.json";
+import GitHub from "./assets/general/GitHub.json";
+import Telegram from "./assets/general/Telegram.json";
+import Twitter from "./assets/general/Twitter.json";
 import General from "./assets/General.json";
-import Telegram from "./assets/Telegram.json";
-import Tencent from "./assets/Tencent.json";
-import Twitter from "./assets/Twitter.json";
-import GitHub from "./assets/GitHub.json";
-import iCloud from "./assets/iCloud.json";
+/**
+ * Special Parts
+ */
+import Block from "./assets/Block.json";
 import OpenAI from "./assets/OpenAI.json";
+import Netflix from "./assets/Netflix.json";
 import Speedtest from "./assets/Speedtest.json";
 
 type RuleObj = {
@@ -18,6 +22,7 @@ type RuleObj = {
     Total: number;
     DOMAIN: number;
     "DOMAIN-SUFFIX": number;
+    "DOMAIN-SET": number;
     "IP-ASN": number;
     "URL-REGEX": number;
     "DOMAIN-KEYWORD": number;
@@ -29,6 +34,7 @@ const defaultInfo = {
   Total: 0,
   DOMAIN: 0,
   "DOMAIN-SUFFIX": 0,
+  "DOMAIN-SET": 0,
   "IP-ASN": 0,
   "URL-REGEX": 0,
   "DOMAIN-KEYWORD": 0,
@@ -62,7 +68,7 @@ const parseDataToRuleObj = (data: Record<string, string[]>): RuleObj => {
 
 const bunWrite = (fileName: string, obj: RuleObj) => {
   // fix path may garbled, it is bug for bun
-  const destination = join(parse(resolve("../Rules")).ext, `${fileName}.list`);
+  const destination = join(resolve("../Rules"), `${fileName}.list`);
   console.log(`[Start]: Building ${fileName}.list ......`);
   console.log("[Destination]:", destination);
   write(
@@ -74,7 +80,7 @@ const bunWrite = (fileName: string, obj: RuleObj) => {
       }),
       `# Updated: ${new Date().toString()}\n`,
       ...obj.value,
-    ].join("\n")
+    ].join("\n"),
   );
 };
 
@@ -84,7 +90,6 @@ const buildGeneral = () => {
   [
     parseDataToRuleObj(GitHub),
     parseDataToRuleObj(Google),
-    parseDataToRuleObj(Netflix),
     parseDataToRuleObj(Telegram),
     parseDataToRuleObj(Twitter),
     parseDataToRuleObj(General),
@@ -92,6 +97,7 @@ const buildGeneral = () => {
     info.Total += it.info.Total;
     info.DOMAIN += it.info.DOMAIN;
     info["DOMAIN-SUFFIX"] += it.info["DOMAIN-SUFFIX"];
+    info["DOMAIN-SET"] += it.info["DOMAIN-SET"];
     info["IP-ASN"] += it.info["IP-ASN"];
     info["URL-REGEX"] += it.info["URL-REGEX"];
     info["DOMAIN-KEYWORD"] += it.info["DOMAIN-KEYWORD"];
@@ -103,30 +109,8 @@ const buildGeneral = () => {
   });
 };
 
-const buildChina = () => {
-  const info = { ...defaultInfo };
-  const value = [];
-  [parseDataToRuleObj(Tencent), parseDataToRuleObj(China)].forEach((it) => {
-    info.Total += it.info.Total;
-    info.DOMAIN += it.info.DOMAIN;
-    info["DOMAIN-SUFFIX"] += it.info["DOMAIN-SUFFIX"];
-    info["IP-ASN"] += it.info["IP-ASN"];
-    info["URL-REGEX"] += it.info["URL-REGEX"];
-    info["DOMAIN-KEYWORD"] += it.info["DOMAIN-KEYWORD"];
-    value.push(...it.value);
-  });
-  bunWrite("China", {
-    info,
-    value,
-  });
-};
-
 buildGeneral();
-buildChina();
 bunWrite("Block", parseDataToRuleObj(Block));
 bunWrite("Netflix", parseDataToRuleObj(Netflix));
-bunWrite("Telegram", parseDataToRuleObj(Telegram));
-bunWrite("Twitter", parseDataToRuleObj(Twitter));
-bunWrite("iCloud", parseDataToRuleObj(iCloud));
 bunWrite("OpenAI", parseDataToRuleObj(OpenAI));
 bunWrite("Speedtest", parseDataToRuleObj(Speedtest));
