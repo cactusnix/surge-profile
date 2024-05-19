be851dcaddf3ff1e6dc83ac5330dd6a05b7789e2
cactusnix <cocalemontea@gmail.com>
2024-05-19 22:31:05 +0800

    feat: temp add rust fiel

--- rules-generator/src/main.rs
+++ rules-generator/src/main.rs
@@ -1,3 +1,17 @@
+use jsonc_parser::parse_to_serde_value;
+use std::fs;
+
 fn main() {
-  println!("Hello, world!");
+  let path = "./assets/openai.jsonc";
+  let openai: String = fs::read_to_string(path).expect("A");
+  // let a = parse_to_serde_value(openai.match, &Default::default());
+  println!("{:?}", openai);
+  Ok(());
+  // match a {
+  //   Ok(value) => {
+  //   },
+  //           Err(e) {
+  //           eprintln!("DD")
+  //       }
+  // }
 }
